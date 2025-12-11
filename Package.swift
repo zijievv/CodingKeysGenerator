51da1cd6f613c4329f880ad872790f9b7af46427
MidnightRocket <git@midnightrocket.dev>
2025-12-11 16:56:12 +0100

    Update dependency: swift-syntax to 602

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "601.0.0-latest")
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", "600.0.1"..<"603.0.0")
     ],
     targets: [
         .target(name: "Shared"),
